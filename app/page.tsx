8170d17d04ddf3eb0aed28fee4e6210448db4c8e
Evgene <>
2023-09-19 18:28:27 +0200

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 import Footer from '@/app/components/footer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-grow p-24">
-        <p className="p-4">Hello world</p>
-      </main>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)',
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <Link className="btn btn-primary" href="/about">
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
